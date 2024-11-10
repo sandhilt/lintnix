7a85a61ecdef4affa47fb9adfac920d05bd0eaaa
sandhilt <6170125+sandhilt@users.noreply.github.com>
2024-11-10 00:48:37 -0300

    feat: some updates

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,39 @@
-use std::{env, error::Error, path::Path};
+use rnix::{
+    ast::{AstNode, AstToken},
+    tokenize, Root, SyntaxKind,
+};
+use std::{any::Any, collections::HashSet, error::Error, fs, hash::Hash, path::Path};
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
-    let path = env::var("NIX_PATH")?;
-    let nixpkgs = path
-        .split(":")
-        .find(|s| s.starts_with("nixpkgs="))
-        .ok_or("nixpkgs not found")?;
 
-    println!("nixpkgs: {}", nixpkgs);
+    let path = Path::new("example/configuration.nix");
+    assert!(path.exists());
 
-    let path_slice = nixpkgs
-        .strip_prefix("nixpkgs=")
-        .ok_or("nixpkgs prefix not found")?;
+    let content = fs::read_to_string(path)?;
 
-    finder(Path::new(path_slice))
-}
+    // let mut identifiers = HashSet::new();
 
-fn finder(path: &Path) -> Result<(), Box<dyn Error>> {
-    let is_dir = path.is_dir();
+    // let tokens = tokenize(&content);
+    // for tk in tokens.iter() {
+    //     println!("token={:?}", tk);
+    //     if tk.0 == SyntaxKind::TOKEN_IDENT {
+    //         identifiers.insert(tk.1);
+    //     }
+    // }
 
-    let mut files = vec![];
-    println!("{:?} is_dir: {}", path, is_dir);
+    // println!("identifiers={:?}", identifiers);
 
-    let mut dirs = path
-        .read_dir()?
-        .map(|e| e.map(|e| e.path()))
-        .collect::<Result<Vec<_>, _>>()?;
+    let parse_root = Root::parse(&content);
+    let root = parse_root.ok()?;
 
-    while let Some(path) = dirs.pop() {
-        let is_dir = path.is_dir();
-        let is_sym = path.is_symlink();
-        let is_file = path.is_file();
-        println!("{:?} is_dir: {}, is_symlink: {}", path, is_dir, is_sym);
+    println!("root={:?}", root);
 
-        if !is_sym {
-            if is_dir {
-                dirs.push(path.clone());
-            }
-
-            if is_file {
-                let ext = path.extension().and_then(|s| s.to_str());
-                if ext == Some("nix") {
-                    files.push(path);
-                }
-            }
-        }
-    }
-
-    println!("files: {:?}", files);
+    // let token_strings = tokens
+    //     .iter()
+    //     .map(|(tk, content)| format!("tk={:?}, content={}", tk, content))
+    //     .collect::<Vec<_>>();
+    // fs::write("output.txt", token_strings.join("\n"))?;
 
     Ok(())
 }
